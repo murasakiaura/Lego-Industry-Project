f2ef90e69fb1593766981fcaf20f90e077530358
Russ07 <90725405+Russ07@users.noreply.github.com>
2024-08-18 15:49:45 +0100

    bug fix

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ function App() {
     <BrowserRouter>
       <Header />
       <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/gift-finder" element={<GiftFinder/>} />
+      {/* <Route path="/" element={<LandingPage />} />
+      <Route path="/gift-finder" element={<GiftFinder/>} /> */}
       </Routes> 
      </BrowserRouter>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,51 +5,44 @@ import logo from "../../assets/images/logo.png";
 import searchicon from "../../assets/images/search-icon.jpg";
 import hearticon from "../../assets/images/heart-icon.png";
 import shoppingbagicon from "../../assets/images/shoppingbag-icon.jpg";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 
 function Header() {
   return (
     <header className="header">
       {/* Logo Link */}
-      <Link to="/" className="header__link">
+      {/* <Link to="/" className="header__link">
         <img className="header__logo" src={logo} alt="Logo" />
-      </Link>
+      </Link> */}
 
       {/* Top Header Links */}
-      <div className="header-top">
-        <a href="#" className="header-link">Find a Store</a>
-        <a href="#" className="header-link">Customer Service</a>
-        <a href="#" className="header-link">United Kingdom</a>
-      </div>
+      {/* <div className="header-top">
+        <p href="" className="header-link">Find a Store</p>
+        <p href="#" className="header-link">Customer Service</p>
+        <p href="#" className="header-link">United Kingdom</p>
+      </div> */}
 
       {/* Main Navigation */}
-      <div className="header-container">
-        <div className="logo">
-          <a href="#">
-            <img src={logo} alt="LEGO Logo" />
-          </a>
-        </div>
+      {/* <div className="header-container"> */}
+        {/* <div className="logo"> */}
+        <Link to="/" className="header__link">
+        <img className="header__logo" src={logo} alt="Logo" />
+      </Link>
+        {/* </div> */}
         <nav>
-          <ul className="nav-list">
-            <li><a href="#">SHOP</a></li>
-            <li><a href="#">DISCOVER</a></li>
-            <li><a href="#">HELP</a></li>
-            <li><a href="#">DREAMZZZ</a></li>
+          <ul className="header__nav-list">
+            <li><p href="#">SHOP</p></li>
+            <li><p href="#">DISCOVER</p></li>
+            <li><p href="#">HELP</p></li>
+            <li><p href="#">DREAMZZZ</p></li>
           </ul>
         </nav>
-        <div className="header-right">
-          <a href="#">
-            <img src={searchicon} alt="Search Icon" />
-          </a>
-          <a href="#">
-            <img src={hearticon} alt="Heart Icon" />
-          </a>
-          <a href="#">
-            <img src={shoppingbagicon} alt="Shoppingbag Icon" />
-          </a>
-          <h4>(0)</h4>
-        </div>
-      </div>
+        {/* <div className="header-right"> */}
+            <FontAwesomeIcon icon={faSearch} />
+        {/* </div> */}
+      {/* </div> */}
     </header>
   );
 }
